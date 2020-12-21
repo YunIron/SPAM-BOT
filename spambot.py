45ef7b69aedbdcde28bc56d10e6d024f04cf7854
YunIron <69750956+YunIron@users.noreply.github.com>
2020-12-21 23:34:54 +0300

    Add files via upload

---
+++ spambot.py
@@ -0,0 +1,53 @@
+from color import *
+import pyautogui
+import time
+
+class spambot:
+    def __init__(self):
+        try:
+            print(f"{yellow}\t\t\t\t\tHosgeldiniz! Bu bir spam botudur.")
+            print(f"{red}\t\t\t\t\t\t\tir0n")
+            print(normal)
+            self.text = str(input("Istediginiz yaziyi yazin sonra nereye yazmak istiyorsaniz oraya tiklayiniz:    "))
+            self.count = int(input("Miktar: "))
+            self.hiz = float(input("Ne kadar surede atsin?: "))
+            self.secim = str(input("Baslatilsin mi?(E/H):    "))
+            self.menu(self.text, self.count, self.secim)
+        except Exception:
+            print('Yanlis deger!')
+            secim = str(input(f"{red}[ - ]Yeniden baslatilsin mi ? ya da kapatilsin mi? {yellow}(Y = Yeniden Baslat, K = Kapat):\t\t"))
+            if secim == "y" or secim == "Y":
+                spambot()
+            elif secim == "k" or secim == "K":
+                exit()
+            else:
+                print("Yanlis Deger!")
+                exit()
+
+    def menu(self, text, count, secim):
+        try:
+            if self.secim == "e" or self.secim == "E":
+                sure = 5
+                for x in range(5):
+                    print(f"{cyan}",sure)
+                    time.sleep(1)
+                    sure -=1
+                    if sure == 0:
+                        print(f"{green}Baslatildi!")
+                        self.spam(self.text, self.count, self.hiz)
+
+            elif secim == "h" or secim == "H":
+                print('Cikis yapiliyor')
+                exit()
+
+        except Exception:
+            print("Yanlis Deger!")
+            (menu)
+
+    def spam(self, text, count, hiz):
+        for x in range(self.count):
+            pyautogui.typewrite(self.text)
+            pyautogui.press('enter')
+            time.sleep(self.hiz)
+
+spambot()
